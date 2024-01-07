caafdb0d6cb3eb13c03e6533331aa01ff05de006
dinho11701 <dinho11701@ubuntu.myguest.virtualbox.org>
2024-01-07 15:34:56 -0500

    ajout liste chainee

---
+++ LinkedList.h
@@ -0,0 +1,20 @@
+struct Node{
+   
+   int value;
+   Node* next;
+   
+   Node(int val) : value(val), next(nullptr) {}
+
+};
+
+
+class LinkedList{
+
+   private:
+      Node* head;
+   
+   
+   public:
+      LinkedList() : {}
+
+};
